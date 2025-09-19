ed32a85f1dde3bc6ef0c74609c9364574f6c5c1e
taowei <1536452459@qq.com>
2025-09-19 18:29:06 +0800

    fix: 修复无字体子集构建报错

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -35,7 +35,10 @@
 import { Inter, Lusitana, Noto_Sans_SC } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
-export const lusitana = Lusitana({ weight: ["400", "700"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
 export const notoSansSC = Noto_Sans_SC({
   subsets: ["latin"], // Noto Sans SC 只支持 latin subset
   weight: ["300", "400", "500", "700"],
